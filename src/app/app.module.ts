68e93aec35771b30bb4668552bd5072e33ea869b
Eduardo Aparecido de Oliveira Junior <tp.eduardo.junior@sp01.local>
2020-06-27 17:40:13 -0300

    Criação do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+//using 
+import { BrowserModule } from '@angular/platform-browser'; //vai rodar via browser
+import { NgModule} from '@angular/core'; //criando o modulo
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+//
+import { AppComponent } from './app.component'; //componente raiz
 
+//Metadata - @ = Decorador -> gera informações adicionais (extension c#)
 @NgModule({
-  declarations: [
+  declarations: [ //quais componentes
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //o que é extra
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //o que vai ser provido para os componentes
+  bootstrap: [AppComponent] //Que componente vai iniciar
 })
+//export = public c#
 export class AppModule { }
